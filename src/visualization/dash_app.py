a0d20dc6490cd0f9762575533adbde71b345aaec
Katharina Klat <61712904+katklat@users.noreply.github.com>
2025-06-10 10:21:01 +0200

    Add first styling

--- src/visualization/dash_app.py
+++ src/visualization/dash_app.py
@@ -1,19 +1,17 @@
 import dash 
-from dash import html
-from dash import dcc
-import pandas as pd
+from dash import html, dcc
+import dash_bootstrap_components as dbc
 from pymongo import MongoClient
+import pandas as pd
 
 
+client = MongoClient("mongodb://crypto_project:dst123@crypto_mongo:27017/")
+db = client["cryptobot"]            
+collection = db["historical_data"]    
 
 
-# Connect to MongoDB
-client = MongoClient('mongodb://localhost:27017/')
-db = client['cryptobot']
-collection = db['historical_data']
-
-
-app = dash.Dash(__name__)
+# BOOTSTRAP, CERULEAN, DARKLY, FLATLY, LITERA, LUX, MATERIA, MINTY, PULSE, SANDSTONE, SIMPLEX, SKETCHY, SLATE, SOLAR, SPACELAB, UNITED
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 app.layout =  html.Div(children = " Crypto Dash API")
 
 if __name__ == '__main__':
