5b3fa852bcd58e7ba5da2d8f6346c5917e0a36eb
Katharina Klat <61712904+katklat@users.noreply.github.com>
2025-07-15 11:03:32 +0200

    Fix import for dash container

--- src/api_user/visualization/dash_app.py
+++ src/api_user/visualization/dash_app.py
@@ -1,10 +1,11 @@
 import sys
 from pathlib import Path
-from .layout.main_layout import layout
 
 # Add the project root to the Python path
 project_root = Path(__file__).parent.parent.parent.parent
 sys.path.insert(0, str(project_root))
+
+from src.api_user.visualization.layout.main_layout import layout
 import dash
 import dash_bootstrap_components as dbc
 
